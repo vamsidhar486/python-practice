f81f7c55be638e2838e811611a0df2457740f5e4
Vamsi <vamsidhar486@gmail.com>
2024-02-08 23:59:52 +0530

    BST Dev

--- DSAlgorithmsPractice/Trees/BST.py
+++ DSAlgorithmsPractice/Trees/BST.py
@@ -43,7 +43,8 @@ class BST:
     if data is smaller search in left sub tree (left can be empty / left can have one or more nodes)
     if data is larger then search right sub tree (right can be empty / right can have more nodes)
     """
-    def search(self,data):
+
+    def search(self, data):
         if self.key == data:
             print("Node is found")
             return
@@ -58,7 +59,37 @@ class BST:
             else:
                 print("Node is not present")
 
+    """
+    first root --> left --> right
+    print root key, left key and the nright key
+    """
 
+    def preorder(self):
+        print(self.key, end=" ")
+        if self.lchild:
+            self.lchild.preorder()
+        if self.rchild:
+            self.rchild.preorder()
+
+    """
+    left --> root --> right same order in left sub tree and right subtree
+    """
+    def inorder(self):
+        if self.lchild:
+            self.lchild.inorder()
+        print(self.key, end=" ")
+        if self.rchild:
+            self.rchild.inorder()
+
+    """
+    left --> right --> root
+    """
+    def postorder(self):
+        if self.lchild:
+            self.lchild.postorder()
+        if self.rchild:
+            self.rchild.postorder()
+        print(self.key, end=" ")
 
 # root = BST(10)
 # print(root)
@@ -83,3 +114,11 @@ for i in list1:
     root.insert(i)
 root.search(6)
 root.search(11)
+print("Pre Order")
+root.preorder()
+print()
+print("In Order")
+root.inorder()
+print()
+print("Post Order")
+root.postorder()
